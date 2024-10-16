61ff5615d397f4f9605ae08656a7c0c894586fdc
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-10-16 19:58:51 +0700

    Fix QR

--- apps/app/src/components/projects/project-qr.tsx
+++ apps/app/src/components/projects/project-qr.tsx
@@ -1,22 +1,19 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { type Address } from "viem";
 import { QRCodeSVG } from "qrcode.react";
 import { Markdown } from "~/components/ui/markdown";
 import { type ReactNode } from "react";
-import { useProjectDetails } from "~/hooks/use-project-details";
 import { useMetadata } from "~/hooks/use-metadata";
 import { ProjectBadge } from "./project-badge";
 import { getBaseUrl } from "~/trpc/react";
 import { ProjectMeta } from "./project-meta";
+import { useProjectById } from "~/hooks/use-projects";
 
 export function ProjectQR({ action = null }: { action: ReactNode }) {
   const { projectAddress } = useParams();
-  const { data: details, isPending } = useProjectDetails(
-    projectAddress as Address,
-  );
-  const { data: metadata } = useMetadata(details?.projectMetadata);
+  const { data: details, isPending } = useProjectById(projectAddress);
+  const { data: metadata } = useMetadata(details?.metadata);
 
   if (isPending) return <div className="h-64 animate-pulse bg-gray-100"></div>;
   if (!details) return <div>Not found</div>;
@@ -25,7 +22,7 @@ export function ProjectQR({ action = null }: { action: ReactNode }) {
     `/projects/${projectAddress}`,
     getBaseUrl(),
   ).toString();
-  const progress = (details.totalFundsRaised / details.fundingTarget) * 100;
+  const progress = (details.totalRaised / details.target) * 100;
   return (
     <>
       <div className="flex items-center gap-1 bg-white py-2">
@@ -34,7 +31,7 @@ export function ProjectQR({ action = null }: { action: ReactNode }) {
           {metadata?.title}
         </h1>
         <div className="flex items-center gap-2">
-          <ProjectBadge projectAddress={projectAddress} />
+          <ProjectBadge id={projectAddress} />
         </div>
       </div>
       <ProjectMeta details={details} />
